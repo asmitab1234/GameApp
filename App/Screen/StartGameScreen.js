321c73c3a5c6299b265b6f21b07a1ee5adb5d120
asmitab1234 <measmitaboral@gmail.com>
2024-02-18 14:30:24 +0530

    up

--- App/Screen/StartGameScreen.js
+++ App/Screen/StartGameScreen.js
@@ -1,12 +1,14 @@
-import { StyleSheet,  TextInput, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 import React from 'react'
 import PrimaryButton from '../Component/primaryButton'
 
 const StartGameScreen = () => {
     return (
         <View
-        style={styles.container}>
-            <TextInput/>
+            style={styles.container}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2} />
             <PrimaryButton>
                 Reset
             </PrimaryButton>
@@ -21,12 +23,28 @@ const StartGameScreen = () => {
 export default StartGameScreen
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         // flex:1,
-        marginHorizontal:24,
-        padding:16,
-        marginTop:100,
-        backgroundColor:'#7C0631',
-        borderRadius:10
+        marginHorizontal: 24,
+        padding: 16,
+        marginTop: 100,
+        backgroundColor: '#810B36',
+        borderRadius: 10,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        borderBottomWidth: 2,
+        borderColor: '#CBA409',
+        fontSize: 24,
+        color: '#CBA409',
+        marginBottom: 10,
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
 })
