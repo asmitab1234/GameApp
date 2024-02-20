0cb26d929c9493f75a3e050c260e02e153029de7
asmitab1234 <measmitaboral@gmail.com>
2024-02-21 00:33:22 +0530

    up

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './App/Screen/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-<>
-<StartGameScreen/>
-</>
+    <LinearGradient
+      colors={['#9C0A47', '#E09711']}
+      style={styles.rootScreen}>
+      <ImageBackground
+      source={{uri:'https://images.unsplash.com/photo-1570303345338-e1f0eddf4946?q=80&w=1971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}}
+      resizeMode='cover'
+      style={styles.rootScreen}
+      imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-
+  rootScreen: {
+    flex: 1
+  },
+  backgroundImage:{
+    opacity:0.25
+  }
 });
